9597ffe0ef6f0f1773ce8c9fbd06676ce6b442ee
pamelagomz <00170221@uca.edu.sv>
2024-01-17 08:31:05 -0600

    :memo:count input

--- creams-client/src/components/CardItem/CardItem.jsx
+++ creams-client/src/components/CardItem/CardItem.jsx
@@ -4,6 +4,9 @@ import { Card, Button } from 'react-bootstrap';
 import imageCoral from '../../assets/Creams-Coral.png'
 import "./carditem.css"
 import "../../index.css"
+import { Box } from '../ProductList/Box';
+import Figure from 'react-bootstrap/Figure';
+
 
 
 const TransportCard = ({ title, description, imageSrc}) => {
@@ -13,8 +16,10 @@ const TransportCard = ({ title, description, imageSrc}) => {
         <Card.Img variant="top" src={imageSrc}/>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button variant="secundary">Go somewhere</Button>
-        <Button variant="primary">Go somewhere</Button>
+        <Box>
+          <Button></Button>
+        </Box>
+        <Button variant="primary">ADD TO CART</Button>
       </Card.Body>
     </Card>
   );
---
+++ creams-client/src/components/ProductList/Box.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./box.css";
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="rectangle" />
+    </div>
+  );
+};
